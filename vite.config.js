7ee969aeac2514b292e88200f129cea57dc9b280
johan0511 <johancasilimas@gmail.com>
2024-06-27 00:15:36 -0500

    Cambios realizados

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-// vite.config.js
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3001,
-  },
-  build: {
-    rollupOptions: {
-      external: ["react-use"],
-    },
-  },
+  server:{
+    port: 3001
+  }
 });
